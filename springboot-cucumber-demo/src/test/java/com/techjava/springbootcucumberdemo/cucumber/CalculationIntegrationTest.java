df6d877f81e0de2c6168375687ba1df21b916c09
keyurgandhi <keyur21.gandhi@gmail.com>
2019-07-17 15:05:20 +0530

    added extent and json report

--- springboot-cucumber-demo/src/test/java/com/techjava/springbootcucumberdemo/cucumber/CalculationIntegrationTest.java
+++ springboot-cucumber-demo/src/test/java/com/techjava/springbootcucumberdemo/cucumber/CalculationIntegrationTest.java
@@ -5,6 +5,11 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", plugin = {"pretty", "html:target/cucumber"}, glue = {"com.techjava.springbootcucumberdemo.cucumber.stepdefs"})
+@CucumberOptions(features = "src/test/resources/features", 
+plugin = {"pretty", 
+		"html:target/cucumber-htmlreport",
+		"json:target/cucumber-report1.json",
+		"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport.html"}, 
+glue = {"com.techjava.springbootcucumberdemo.cucumber.stepdefs"})
 public class CalculationIntegrationTest {
 }
